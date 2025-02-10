e74d5c115ee5d894130e70cd0d342c60fe400526
CHeatiiT <161832522+Shivamborn2shine@users.noreply.github.com>
2025-02-11 02:14:53 +0530

    Update multiplication_of_array.c

--- multiplication_of_array.c
+++ multiplication_of_array.c
@@ -4,7 +4,8 @@ int main(){
     int arr1[50][50],arr2[50][50],mul[50][50];
     int i,j;
     int sum=0;
-    int m=0;
+    int m=0,i2=0,k;
+    int product;
     printf("Enter the dimentions of 1st array----here--> ");
     scanf("%d%d",&r1,&c1);
     printf("Enter the dimentions of 2st array----here--> ");
@@ -22,31 +23,32 @@ int main(){
 printf("\nNow we have the matrices you entered as follows \n");
 printf("1st matrice\n");
  for(i=0; i< (r1*c1); i++){
-		printf("%d",arr1[i/c1][i%c1]);
+		printf("%d ",arr1[i/c1][i%c1]);
         if(i%c1==c1-1){
                 printf("\n") ;
                 }
  }
  printf("2nd matrice\n");
  for(i=0; i< (r2*c2); i++){
-		printf("%d",arr2[i/c2][i%c2]);
+		printf("%d ",arr2[i/c2][i%c2]);
         if(i%c2==c2-1){
                 printf("\n") ;
                 }
  }
  printf("Now Processing the multiplication of the matrices is :\n");
-for(i=0; i<(r1*c1); i++){
-         printf("%d",sum);
-       sum = sum + (arr1[i/c1][i%c1] * arr2[i/c2][i%c2]);
-        if(i%c1==c1-1){
-                mul[m/c2][m%c2] =   sum;   //m is index or iterative for mul as i is being used
-                sum = 0;
-                printf("\n1 row done\n") ;
-                }
-    }
-    printf("multiplication of  the matrices is as follows\n");
+ for (i = 0 ; i < r1; i++){
+   for (j = 0; j < c2; j++){
+     mul[i][j] = 0;
+     for(k = 0; k < c1; k++){
+       mul[i][j] += arr1[i][k] * arr2[k][j];
+     }
+      printf("done ");
+   }
+ }
+
+    printf("\nmultiplication of  the matrices is as follows\n");
  for(i=0; i< (r1*c2); i++){
-		printf("%d",arr1[i/c2][i%c2]);
+		printf("%d ",mul[i/c2][i%c2]);
         if(i%c2==c2-1){
                 printf("\n") ;
                 }
